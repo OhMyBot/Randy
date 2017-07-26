114f05e9385249428ca6215b877f95928e26462a
Gordon Chang <gochang@microsoft.com>
2017-07-26 10:56:05 -0700

    changed default text

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ let rantDb = []
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 const bot = new builder.UniversalBot(connector, session => {
   session.send(
-    "Hi, I'm OhMyBot! You can give me feedback or ask for results with a #hastag."
+    "Hi, my name is OhMyBot! You can give me feedback or ask for results with a #hastag."
   )
 })
 
